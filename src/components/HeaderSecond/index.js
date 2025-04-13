ce685cc4b4bcf4ae6cbd2ec4ab597ec82455d51b
shiva <smpgr2351@gmail.com>
2025-04-13 13:06:33 +0530

    first

--- src/components/HeaderSecond/index.js
+++ src/components/HeaderSecond/index.js
@@ -3,8 +3,8 @@ import "./index.css"
 const HeaderSecond =()=>{
     console.log("header")
     return <nav className="header">
+        <Link to="login"><img className="logo-img" src="https://res.cloudinary.com/delrn2vxa/image/upload/v1744529006/images_tj93cl.jpg" alt="logo" /></Link>
         
-        <Link to="login"><img alt="logo"/></Link>
         <ul className="list">
            
            <li>
